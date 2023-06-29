0c1b66e5ba4a84f3ba48ba33b284dc4165ea5ce3
thl_7 <thl_7@yahoo.com.br>
2023-06-29 17:07:24 -0300

    teste

---
+++ src/main/java/org/primefaces/teste/teste.java
@@ -0,0 +1,12 @@
+package org.primefaces.teste;
+
+import jakarta.persistence.Persistence;
+
+public class teste {
+
+	public static void main(String[] args) {
+		Persistence.createEntityManagerFactory("");
+
+	}
+
+}
