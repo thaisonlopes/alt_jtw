a0f41cc6677180a33cc8cb16138f79ce466bc056
thl_7 <thl_7@yahoo.com.br>
2023-07-06 17:23:26 -0300

    Coisas genéricas acrescentadas

---
+++ src/main/java/org/primefaces/util/base/AltException.java
@@ -0,0 +1,11 @@
+package org.primefaces.util.base;
+
+public class AltException extends RuntimeException {
+
+	private static final long serialVersionUID = -4505872316352282342L;
+
+	public AltException(String msg) {
+        super(msg);
+    }
+
+}
